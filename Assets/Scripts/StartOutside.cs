9bec995be745108eced57502443d0f4402e66f15
kasperja <kasperjensenandersen@gmail.com>
2016-11-28 12:19:17 +0100

    cam start outside, player running in
    cam start outside, player running in

---
+++ Assets/Scripts/StartOutside.cs
@@ -0,0 +1,74 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartOutside : MonoBehaviour {
+
+	public PlatformerCharacter2D pc2D;
+	public Animator m_Anim;
+
+	public GameObject mainCam;
+	public GameObject boxColStart;
+
+
+	// Use this for initialization
+	void Start () {
+
+
+		pc2D.isStartOutside = true;
+		pc2D.startCamBool = true;
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (pc2D.isStartOutside) {
+
+			pc2D.sideArrowsBool = true;
+			m_Anim.SetBool ("SideArrows", true);
+
+		}
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other)
+	{
+
+		if (other.gameObject.tag == "Player") {
+
+			pc2D.isStartOutside = true;
+			pc2D.startCamBool = true;
+		}
+
+
+	}
+
+	void OnTriggerExit2D(Collider2D other)
+	{
+
+		if (other.gameObject.tag == "Player") {
+
+			pc2D.isStartOutside = false;
+
+			boxColStart.SetActive (true);
+		}
+
+
+	}
+
+	void OnTriggerStay2D(Collider2D other)
+	{
+
+		if (other.gameObject.tag == "Player") {
+
+
+			pc2D.isStartOutside = true;
+			pc2D.startCamBool = true;
+
+		}
+
+
+	}
+
+
+}
---
+++ Assets/Scripts/StartScriptCam.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class StartScriptCam : MonoBehaviour {
+
+	public PlatformerCharacter2D pc2D;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	void OnTriggerExit2D(Collider2D other)
+	{
+
+		if (other.gameObject.tag == "Player") {
+
+
+			pc2D.startCamBool = false;
+
+
+		}
+
+
+	}
+}
