f6c6e06b2fb3d79b3b6edf2cd3b54e9a4e7933a2
kasperja <kasperjensenandersen@gmail.com>
2016-12-08 14:55:47 +0100

    fade ambience sound out when ending
    fade ambience sound out when ending

--- Assets/Scripts/EndOutside.cs
+++ Assets/Scripts/EndOutside.cs
@@ -14,6 +14,9 @@ public class EndOutside : MonoBehaviour {
 	public GameObject titleObj;
 	private bool hasEnded = false;
 	public AudioSource footStepsSoundGravel;
+	public AudioSource ambienceOne;
+	public AudioSource ambienceTwo;
+	public bool fadeAmbienceOut = false;
 	// Use this for initialization
 	void Start () {
 	
@@ -25,6 +28,13 @@ public class EndOutside : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 	
+		if (fadeAmbienceOut) {
+		
+			ambienceOne.volume -= 1f * Time.deltaTime;
+			ambienceTwo.volume -= 1f * Time.deltaTime;
+		
+		}
+
 		if (pc2D.isEndOutside) {
 		
 			pc2D.sideArrowsBool = true;
@@ -82,6 +92,7 @@ public class EndOutside : MonoBehaviour {
 		hasEnded = true;
 		yield return new WaitForSeconds (waitBeforeEnd);
 
+		fadeAmbienceOut = true;
 		endFadeAnim.SetBool ("FadeEnd", true);
 
 		yield return new WaitForSeconds (1f);
@@ -91,7 +102,7 @@ public class EndOutside : MonoBehaviour {
 	IEnumerator waitEndTwo(){
 
 		endFadeAnim.SetBool ("FadeEnd", true);
-
+		fadeAmbienceOut = true;
 		yield return new WaitForSeconds (1f);
 		SceneManager.LoadScene (0);
 
